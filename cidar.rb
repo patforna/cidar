37f9f56f1426729e29f402839231b61c01c839e3
Patric Fornasier <patric.fornasier@gmail.com>
2011-08-28 18:18:09 +0100

    pat: initial commit

---
+++ cidar.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+require 'sinatra' 
+
+SERVER_URL = 'http://172.18.20.31:8153/go/cctray.xml'
+
+get '/' do
+  @doc = Nokogiri::XML(open(SERVER_URL))
+  erb :index
+end
+
+helpers do
+  def status_of(project)
+    @status = Status.new(@doc.xpath("//Project[@name='#{project}']").first)
+    erb 'status <%= if @status.success? then "success" else "failure" end %> <%= "building" if @status.building? %>'
+  end
+end
+
+class Status
+  def initialize(node); @node = node end
+  def success?; @node['lastBuildStatus'] == "Success" end
+  def building?; @node['activity'] == "Building" end  
+end
