b31caf71f5ab9f7fb41675ecdb2b50a3c1a078d7
Patric Fornasier <patric.fornasier@gmail.com>
2011-10-04 18:09:47 +0100

    pat: stage aggregation.

--- cidar.rb
+++ cidar.rb
@@ -19,23 +19,36 @@ end
 
 helpers do
   def status_of(project)
+    # puts "getting status for #{project}."    
     @status = Status.new(@doc.xpath(".//Project[regex(., '^#{project}$')]", Class.new {
       def regex node_set, regex
         node_set.find_all { |node| node['name'] =~ /#{regex}/ }
       end
-    }.new).first)
+    }.new))
     erb 'status <%= if @status.success? then "success" else "failure" end %><%= " building" if @status.building? %>'
   end
 end
 
 class Status
-  def initialize(node); @node = node end
-  def success?; @node['lastBuildStatus'] == "Success" end
-  def building?; @node['activity'] == "Building" end
-  def buildLabel; strip_runs @node['lastBuildLabel'] end
+  def initialize(nodes)
+    # puts "nodes: #{nodes}"    
+    @nodes = nodes
+  end
+  
+  def success?
+    @nodes.all? { |node| node['lastBuildStatus'] == "Success" }
+  end
+  
+  def building?
+    @nodes.all? { |node| node['activity'] == "Building" }
+  end
+  
+  def buildLabel
+    strip_runs @nodes.first['lastBuildLabel']
+  end
   
   def commit_message
-    get_commit_message_from_url(@node['webUrl'])  
+    get_commit_message_from_url(@nodes.first['webUrl'])  
   end
   
   def commiters
@@ -45,7 +58,9 @@ class Status
   
   
   private
-  def strip_runs(buildLabel); buildLabel.gsub(/\s.*/, '') end
+  def strip_runs(buildLabel)
+    buildLabel.gsub(/\s.*/, '')
+  end
   
   def get_commit_message_from_url (url)
     web_html = Nokogiri::HTML(open(url))
